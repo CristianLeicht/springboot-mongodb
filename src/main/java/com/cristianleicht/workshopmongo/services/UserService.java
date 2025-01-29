f11419a3dae91937bff0b3e9f1275709eaee708f
Cristian Leicht <cristian.leicht@hotmail.com>
2025-01-29 20:14:01 -0300

    obtendo um usuario por id/tratamento de erro

--- src/main/java/com/cristianleicht/workshopmongo/services/UserService.java
+++ src/main/java/com/cristianleicht/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.cristianleicht.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cristianleicht.workshopmongo.domain.User;
 import com.cristianleicht.workshopmongo.repository.UserRepository;
+import com.cristianleicht.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,17 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+		
+	}
+	
+	public User findById(String id) {
+		Optional<User> user  = repo.findById(id);
+		
+		if(!user.isPresent()) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		
+		return user.get();
 		
 	}
 
