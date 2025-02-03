f8f8bb429db3295d8e9f4c07c8b1e557ee18c4ae
Cristian Leicht <cristian.leicht@hotmail.com>
2025-02-03 19:17:09 -0300

    criado entity Post com User aninhado

---
+++ src/main/java/com/cristianleicht/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.cristianleicht.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cristianleicht.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+}
