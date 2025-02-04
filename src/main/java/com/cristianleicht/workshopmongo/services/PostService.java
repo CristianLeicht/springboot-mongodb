5504e52a957c00caec7cc3943f2bf008d8861a09
Cristian Leicht <cristian.leicht@hotmail.com>
2025-02-04 20:09:42 -0300

    Obtendo post por id

---
+++ src/main/java/com/cristianleicht/workshopmongo/services/PostService.java
@@ -0,0 +1,30 @@
+package com.cristianleicht.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cristianleicht.workshopmongo.domain.Post;
+import com.cristianleicht.workshopmongo.repository.PostRepository;
+import com.cristianleicht.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+
+	public Post findById(String id) {
+		Optional<Post> user = repo.findById(id);
+
+		if (!user.isPresent()) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+
+		return user.get();
+
+	}
+
+}
