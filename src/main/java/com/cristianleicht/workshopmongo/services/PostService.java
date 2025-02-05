82ea72010e116deffe54da5be8a3e8e3b7ad6768
Cristian Leicht <cristian.leicht@hotmail.com>
2025-02-05 20:17:53 -0300

    Consulta simples com query methods

--- src/main/java/com/cristianleicht/workshopmongo/services/PostService.java
+++ src/main/java/com/cristianleicht/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.cristianleicht.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +26,11 @@ public class PostService {
 
 		return user.get();
 
+	}
+	
+	public List<Post> findByTitle(String text){
+		
+		return repo.findByTitleContainingIgnoreCase(text);
 	}
 
 }
