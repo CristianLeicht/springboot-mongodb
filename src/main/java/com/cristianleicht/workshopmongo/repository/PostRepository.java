2ebdd578b2a57759e45e7ff99d5fb66e4fd349ec
Cristian Leicht <cristian.leicht@hotmail.com>
2025-02-05 20:25:50 -0300

    Consulta simples com @Query

--- src/main/java/com/cristianleicht/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/cristianleicht/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.cristianleicht.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.cristianleicht.workshopmongo.domain.Post;
@@ -10,6 +11,12 @@ import com.cristianleicht.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 	
+	
+	//?0 = primeiro parametro que vier no método
+	//'i' = ignoreCase
+	@Query("{'title': {$regex: ?0, $options: 'i'}}")
+	List<Post> searchTitle(String text);
+
 	List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
